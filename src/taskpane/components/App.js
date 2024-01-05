56326b091aa038af85ee164e1efe9b5970a68f90
Muhammad Qadeer <qadeer.kahout53@gmail.com>
2024-01-05 12:39:13 +0500

    changes

---
+++ src/taskpane/components/App.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+function App() {
+
+  return (
+    <>
+      {/* <CustomAvatar
+        imageUrl="https://firebasestorage.googleapis.com/v0/b/acies-cfc88.appspot.com/o/PlatenKnife.png?alt=media&token=0a79e524-67a2-4018-82de-d5c5cccf0553"
+        name="John Doe"
+       */}
+
+    <h1>Hello</h1>
+    </>
+  );
+}
+export default App;
